5b125e3e8b92d39cf348e1604b4813fd26879569
EzazAsif <asifrafid8399@gmail.com>
2024-10-07 23:38:07 +0600

    checked the spam detector and showed it on index.html

--- Buzz/messenger/views.py
+++ Buzz/messenger/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import get_user_model
+from .spamdetector import *
 # Create your views here.
 
 def index(request):
-     
-    return render(request, 'index.html')
+    message="WINNER!! As a valued network customer you have been selected to receivea å£900 prize reward! To claim call 09061701461. Claim code KL341. Valid 12 hours only."
+    pred=makepreds(message)
+    return render(request, 'index.html',{'pred':pred,'message':message})
