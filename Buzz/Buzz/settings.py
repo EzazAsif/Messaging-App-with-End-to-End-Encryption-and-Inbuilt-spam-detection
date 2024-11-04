c07fa3490be9da142e4707764b1516cb8ca4fddd
Ezaz Asif <asifrafid8399@gmail.com>
2024-11-04 17:21:10 +0600

    added csrf trusted origin option

--- Buzz/Buzz/settings.py
+++ Buzz/Buzz/settings.py
@@ -164,3 +164,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+
+
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://<your-ngrok-subdomain>.ngrok.io'
+]
